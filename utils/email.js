c179ae2d25b689dfed38ee75266d8356aca5480a
mahmoud jeilani <jeilani@gmail.com>
2021-12-19 23:26:58 +0000

    added Notification system

--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,23 @@
 
-// require('dotenv').config();
-// const sgMail = require('@sendgrid/mail')
+require('dotenv').config();
+const sgMail = require('@sendgrid/mail')
 
-// const apiKey = process.env.SENDGRID_API_KEY;
-// const email = 'jeilani@gmail.com'
+const apiKey = process.env.SENDGRID_API_KEY;
+const email = 'jeilani@gmail.com'
 
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-// const msg = {
-//   to: 'jeilani@gmail.com', // Change to your recipient
-//   from: 'info@eastberry.io', // Change to your verified sender
-//   subject: 'Sending with SendGrid is Fun',
-//   text: 'and easy to do anywhere, even with Node.js',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// }
-// sgMail
-//   .send(msg)
-//   .then(() => {
-//     console.log('Email sent')
-//   })
-//   .catch((error) => {
-//     console.error(error)
-//   })
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'jeilani@gmail.com', // Change to your recipient
+  from: 'info@eastberry.io', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
